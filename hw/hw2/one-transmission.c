bbf8430b0b4dcff6184f89582c1550b7aa390dbb
Ethan Kamus <ethanjpkamus@csu.fullerton.edu>
2022-02-24 08:13:45 -0800

    wip

--- hw/hw2/one-transmission.c
+++ hw/hw2/one-transmission.c
@@ -10,23 +10,18 @@ int main( int argc, char *argv[] )
 	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
 	MPI_Comm_size( MPI_COMM_WORLD, &size );
 	
-//	float precision = MPI_Wtick();
 	float a = 479;
 
 	// === blocking transmission ===
-	b_starttime = MPI_Wtime(); //MPI_Wtick();	
+	b_starttime = MPI_Wtime();
 	if (rank == 0) {
 		printf("\n=== BEGIN BLOCKING TRANSMISSION ===\n");
 		MPI_Send(&a, 1, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
 		printf("P_0 sent %f to P_1\n", a);
 	} else if (rank == 1) {
 		MPI_Recv(&a, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		b_endtime = MPI_Wtime(); //MPI_Wtick();
+		b_endtime = MPI_Wtime();
 		printf("P_1 received %f from P_0\n", a);
-		
-//		b_starttime *= precision;
-//		b_endtime *= precision;
-//		printf("start: %Lf\nend: %Lf\n",b_starttime, b_endtime);
 		printf("Total Time: %f (seconds)\n", b_endtime-b_starttime);
 	}
 	
--- hw/hw2/two-transmission.c
+++ hw/hw2/two-transmission.c
@@ -4,20 +4,16 @@
 // passing array of ints between processes
 int main( int argc, char *argv[] )
 {
-	
 	int rank, size;
-	float starttime, endtime;
+	double starttime, endtime;
 	MPI_Init( &argc, &argv );
 	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
 	MPI_Comm_size( MPI_COMM_WORLD, &size );
 	
 	float a = 3;
-//	float precision = MPI_Wtick();
-//	printf("precision: %.10f\n", precision);
-//	starttime = MPI_Wtime();
+	starttime = MPI_Wtime();
 	if (rank == 0) {
 		printf("\n=== BEGIN BLOCKING TRANSMISSIONS (2 TRANSMISSIONS) ===\n");
-		starttime = MPI_Wtime() * MPI_Wtick();
 		
 		//send to 1
 		MPI_Send(&a, 1, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
@@ -26,11 +22,9 @@ int main( int argc, char *argv[] )
 		//receive from 1
 		MPI_Recv(&a, 1, MPI_FLOAT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		printf("P_0 received %f from P_1\n", a);
-		endtime = MPI_Wtime() * MPI_Wtick();
+		endtime = MPI_Wtime();
 
-//		starttime *= precision;
-//		endtime *= precision;
-		printf("start: %f\nend: %f\n", starttime, endtime);
+		// printf("start: %f\nend: %f\n", starttime, endtime);
 		printf("Total Time: %f (seconds)\n", endtime-starttime);
 	} else if (rank == 1) {
 		//receive from 0
@@ -41,14 +35,7 @@ int main( int argc, char *argv[] )
 		MPI_Send(&a, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
 		printf("P_1 sent %f to P_0\n", a);
 	}
-//	if (rank == 0) {
-//		endtime = MPI_Wtime();
-//		starttime *= precision;
-//		endtime *= precision;
 
-//		printf("start: %f\nend: %f\n", starttime, endtime);
-//		printf("Total Time: %f\n", endtime-starttime);
-//	}
 	MPI_Finalize();
 	return 0;
 }
