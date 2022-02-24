c62f3d1370793aeae9c7770bc89ca5aade94bd23
Ethan Kamus <ethanjpkamus@csu.fullerton.edu>
2022-02-23 17:23:31 -0800

    fixed one-transmission.c

--- hw/hw2/one-transmission.c
+++ hw/hw2/one-transmission.c
@@ -5,7 +5,7 @@
 int main( int argc, char *argv[] )
 {
 	int rank, size;
-	long double b_starttime, b_endtime;
+	double b_starttime, b_endtime;
 	MPI_Init( &argc, &argv );
 	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
 	MPI_Comm_size( MPI_COMM_WORLD, &size );
@@ -14,22 +14,22 @@ int main( int argc, char *argv[] )
 	float a = 479;
 
 	// === blocking transmission ===
+	b_starttime = MPI_Wtime(); //MPI_Wtick();	
 	if (rank == 0) {
 		printf("\n=== BEGIN BLOCKING TRANSMISSION ===\n");
-		b_starttime = MPI_Wtime() * (long double)(MPI_Wtick());	
 		MPI_Send(&a, 1, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
 		printf("P_0 sent %f to P_1\n", a);
 	} else if (rank == 1) {
 		MPI_Recv(&a, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		b_endtime = MPI_Wtime() * (long double)(MPI_Wtick());
+		b_endtime = MPI_Wtime(); //MPI_Wtick();
 		printf("P_1 received %f from P_0\n", a);
 		
 //		b_starttime *= precision;
 //		b_endtime *= precision;
-		printf("start: %Lf\nend: %Lf\n",b_starttime, b_endtime);
-		printf("Total Time: %Lf (seconds)\n", b_endtime-b_starttime);
+//		printf("start: %Lf\nend: %Lf\n",b_starttime, b_endtime);
+		printf("Total Time: %f (seconds)\n", b_endtime-b_starttime);
 	}
-
+	
 	MPI_Finalize();
 	return 0;
 }
