ed7dbfd1461cbcd59c920aa5872d2472f8885b8f
Ethan Kamus <ethanjpkamus@csu.fullerton.edu>
2022-03-08 09:37:16 -0800

    random working

--- hw/hw3/inclusive-scan.c
+++ hw/hw3/inclusive-scan.c
@@ -7,26 +7,24 @@
 int main( int argc, char *argv[] )
 {
 	int rank, size;
-    int n = 8;
+    int n = 1;
+    srand(time(NULL));
 	MPI_Init( &argc, &argv );
 	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
 	MPI_Comm_size( MPI_COMM_WORLD, &size );
-	// int send[n];
-    // int recv[n];
-    int send[] = {3,1,7,1,4,1,6,3};
-    int recv[] = {0,0,0,0,0,0,0,0};
-    int r;
-	
-    MPI_Scan(&send[rank], &r, n, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-    
 
-    // print recv for the sake of testing (dont include in the final program)
-    // printf("i am rank %d with [", rank);
-    // for(int i = 0; i < n-2; ++i) {
-    //     printf("%d, ", recv[i]);
-    // }
-    // printf("%d]\n",recv[n-1]);
-    printf("i am process %d and recieve range: %d - %d\n", rank, (r-send[rank])+1, r);
+    // hard coded send buffer
+    // int send[] = {3,1,7,1,4,1,6,3};
+    int send = rand() % 100;
+    int r;
+    // scan will implicitly know all the previous calculations of r
+    // so dont need recv buffer as an array
+    // int recv[] = {0,0,0,0,0,0,0,0};
+
+    // MPI_Scan(&send[rank], &r, n, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    // printf("i am process %d and recieve range: %d - %d\n", rank, (r-send[rank])+1, r);
+	MPI_Scan(&send, &r, n, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    printf("i am process %d and recieve range: %d - %d\n", rank, (r-send)+1, r);
 	MPI_Finalize();
 	return 0;
 }
