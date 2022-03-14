b4faf49697b37e286a9cc40a32cb8ea7eeda3098
Ethan Kamus <ethanjpkamus@csu.fullerton.edu>
2022-03-14 10:11:59 -0700

    finished ex3

--- hw/hw4/ex3.c
+++ hw/hw4/ex3.c
@@ -8,17 +8,22 @@ int main(int argc, char **argv)
 {
     int array[N];
     int i, tid;
+    int sum = 0;
 
-    #pragma omp parallel private(i, tid) shared(array) num_threads(8)
+    #pragma omp parallel private(i, tid) shared(array, sum) num_threads(8)
     {
         tid = omp_get_thread_num();
 
         #pragma omp for
         for (i = 0; i < N; i++)
         {
-            array[i] = 0;
+            array[i] = i;
             printf("thread %d assigning %d to array[%d]\n",tid, array[i], i);
+            
+            #pragma omp critical
+            sum += (array[i]%2 == 0 ? 1 : 0 );
         }
     }
+    printf("there are %d even numbers\n", sum);
     return 0;
 }
--- hw/hw4/r.sh
+++ hw/hw4/r.sh
@@ -10,6 +10,7 @@ echo '=== exercise 2 ==='
 gcc -std=c99 ex2.c -o ex2.out -fopenmp
 ./ex2.out
 
-# echo '=== exercise 3 ==='
-# gcc -std=c99 ex3.c -o ex3.out -fopenmp
-# ./ex3.out
+echo
+echo '=== exercise 3 ==='
+gcc -std=c99 ex3.c -o ex3.out -fopenmp
+./ex3.out
